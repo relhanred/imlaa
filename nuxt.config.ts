5ee87c03836a928f5868791e0b74b6da900cc015
Redha El hanti <relhanti@gmail.com>
2025-03-30 10:44:56 +0200

    fix: tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts'],
-
-  // Configuration CSS
-  css: ['~/assets/css/main.css'],
-
-  // Postcss pour Tailwind
+  modules: [
+    '@nuxt/fonts'
+  ],
+  css: [
+    '~/assets/css/tailwind.css'
+  ],
   postcss: {
     plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  // Variables d'environnement Firebase
-  runtimeConfig: {
-    public: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY || '',
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID || '',
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-      firebaseAppId: process.env.FIREBASE_APP_ID || '',
-      openaiApiKey: process.env.OPENAI_API_KEY || '',
+      '@tailwindcss/postcss': {},
+      autoprefixer: {}
     }
-  },
+  }
 })
